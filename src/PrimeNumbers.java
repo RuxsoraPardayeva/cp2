81189d83f0ed2392a14266c5693c0f4d512286a0
RuxsoraPardayeva <250268@newuu.uz>
2026-02-12 22:24:48 +0500

    Problem 5

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,20 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        for (int num = 2; num <= 100; num++) {
+
+            boolean isPrime = true; // reset flag for each number
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
